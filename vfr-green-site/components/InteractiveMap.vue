1c8430aa53e7390e0f836ed237605a7a6406c070
Jason Carter <jcarter@naturalhues.com>
2020-04-18 21:14:23 -0400

    Working on adding Leaf maps

---
+++ vfr-green-site/components/InteractiveMap.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        test map
+        {{ message }}
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import L from 'leaflet'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+
+export default Vue.component("interactivemap", {
+  data: function() {
+      return {"message": "Hello world"};
+  }
+})
+</script>
--- vfr-green-site/pages/index.vue
+++ vfr-green-site/pages/index.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    Test
+    <interactivemap />
+    test content
+    {{ message }}
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import "~/components/InteractiveMap.vue"
 
 export default Vue.extend({
-  
+  data: function() {
+      return {"message": "Hello world2"};
+  }
 })
 </script>
