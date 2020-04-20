d5481e6062dbe38617ecbfa1ee19b8c74fee3ef6
Jason Carter <jcarter@naturalhues.com>
2020-04-20 16:18:52 -0400

    Still experimenting

--- vfr-green-site/components/InteractiveMap.vue
+++ vfr-green-site/components/InteractiveMap.vue
@@ -2,17 +2,45 @@
     <div>
         test map
         {{ message }}
+        <div id="cesiumContainer"></div>
     </div>
 </template>
 
-<script lang="ts">
+<script lang="javascript">
 import Vue from 'vue'
-import L from 'leaflet'
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import VueCesium from 'vue-cesium'
+import lang from 'vue-cesium/lang/en-us'
+
+Vue.use(VueCesium);
+var viewer = new Cesium.Viewer('cesiumContainer');
 
 export default Vue.component("interactivemap", {
-  data: function() {
-      return {"message": "Hello world"};
-  }
+    data: function() {
+        return {"message": "Hello world"};
+    },
+    mounted: function() {
+        // Create the webglearth script
+        //let mapScript2 = document.createElement("script");
+        //mapScript2.setAttribute("src", "http://www.webglearth.com/v2/api.js");
+        //document.head.appendChild(mapScript2);
+
+        // Initialize the map
+        //let myMap = L.map('mapid');
+        /*
+        myMap.setView(L.latLng(43, -71), 0);
+        L.tileLayer('http://localhost:3000/{z}/{x}/{y}.png', {
+        maxZoom: 18
+        }).addTo(myMap);
+        */
+
+        // Go here to understand zoom levels and tile sizes: https://leafletjs.com/examples/zoom-levels/
+    }
 })
 </script>
+
+<style scoped>
+.viewer {
+    width: 100%;
+    height: 400px;
+}
+</style>
