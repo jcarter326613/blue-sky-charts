99b5e458ccf726d58dac43d1eb1e78c076069059
Jason Carter <jcarter@naturalhues.com>
2020-06-01 22:15:58 -0400

    Changed bucket name

--- geotiff-map-exploder/explode_maps.py
+++ geotiff-map-exploder/explode_maps.py
@@ -87,7 +87,7 @@ def explode_maps(map_name, location_name):
             os.makedirs("../vfr-green-site/static/maps/world-vfr/sectional")
         os.system("mv {} ../vfr-green-site/static/maps/world-vfr/sectional".format(image_cache_folder))
     elif location_name == "remote":
-        os.system("aws s3 sync ./maps/tiles s3://vfr-green-artifacts-245819277863/maps/world-vfr/sectional")
+        os.system("aws s3 sync ./maps/tiles s3://blueskycharts.com/maps/world-vfr/sectional")
     #os.system("rm -f {}*".format(png_image_path))
 
 if len(sys.argv) != 3:
