2afff1e958c11f992e76f4f8722fdcbf2cae1658
Jason Carter <jcarter@naturalhues.com>
2020-06-10 11:40:37 -0400

    Copied EasyAwait over to this library

---
+++ easy-await/src/main/easy-await.ts
@@ -0,0 +1,84 @@
+export class EasyAwait {
+    public static instance: EasyAwait = new EasyAwait();
+    private promise: Promise<void> | undefined;
+    private threadCount: number;
+    private threadLocationCount: Record<string, number>;
+    private started: boolean;
+    private fatalErrorReported: boolean;
+    private fatalErrorMessage: string | undefined;
+
+    private constructor() {
+        this.started = false;
+        this.threadCount = 0;
+        this.threadLocationCount = {};
+        this.promise = undefined;
+        this.fatalErrorReported = false;
+    }
+
+    public initialize(): void {
+        this.started = false;
+        this.threadCount = 0;
+        this.promise = new Promise<void>((resolve) => {
+            this.wait(resolve);
+        });
+    }
+
+    public startThread(location: string): void {
+        if ( this.hasFatalError() ) {
+            return;
+        }
+        if ( !(location in this.threadLocationCount) ) {
+            this.threadLocationCount[location] = 0;
+        }
+        this.threadLocationCount[location]++;
+        this.threadCount++;
+        this.started = true;
+    }
+
+    public endThread(location: string): void {
+        if ( this.hasFatalError() ) {
+            return;
+        }
+        if ( !(location in this.threadLocationCount) ) {
+            this.reportFatalError(`EasyAwait, location ${location} ended but never started`);
+            return;
+        }
+        if ( this.threadLocationCount[location] == 0 ) {
+            this.reportFatalError(`EasyAwait, location ${location} ended but is unbalanced`);
+            return;
+        }
+        this.threadLocationCount[location]--;
+        this.threadCount--;
+        if ( this.threadCount < 0 ) {
+            this.reportFatalError("EasyAwait thread count unbalanced in unknown location.");
+        }
+    }
+
+    public async join(): Promise<void> {
+        await this.promise;
+    }
+
+    public hasFatalError(): boolean {
+        return this.fatalErrorReported;
+    }
+
+    public getFatalMessage(): string | undefined {
+        return this.fatalErrorMessage;
+    }
+
+    public reportFatalError(logMessage: string, publicMessage: string | undefined = undefined): void {
+        console.error(logMessage);
+        this.fatalErrorReported = true;
+        this.fatalErrorMessage = publicMessage;
+        this.threadCount = 0;
+        this.started = true;
+    }
+
+    private wait(resolve: () => void): void {
+        if (!this.started || this.threadCount > 0) {
+            setTimeout(() => this.wait(resolve), 1);
+        } else {
+            resolve();
+        }
+    }
+}
---
+++ easy-await/src/main/index.ts
@@ -0,0 +1,1 @@
+export {EasyAwait} from './easy-await';
