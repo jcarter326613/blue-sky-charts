f6ad9f77fa2a4a5b537b87db93971d0f4226ce51
Jason Carter <jcarter@naturalhues.com>
2020-06-26 21:52:26 -0400

    Before cache test

--- geotiff-mosaic/src/main/tile-cache.ts
+++ geotiff-mosaic/src/main/tile-cache.ts
@@ -1,14 +1,34 @@
+import { exec } from 'child_process'
 import { SubMapDescription } from "./sub-map-description";
+import { exit } from 'process';
 
 export class TileCache {
-    /*
-    public generate(mapNames: string[], zoom: number): void {
-
-    }
-    */
+    private generatedFiles: Record<string, Record<number, string>> = {}     //mapname, zoom, root directory
 
     public getPathForTile(subMapDescription: SubMapDescription, targetZoom: number, x: number, y: number) {
-        return `./${subMapDescription.name}/${targetZoom}/${x}_${y}.png`
+        // Return the path if it's been generated
+        if ( this.generatedFiles[subMapDescription.name] !== undefined && 
+            this.generatedFiles[subMapDescription.name][targetZoom] !== undefined ) {
+
+            return `${this.generatedFiles[subMapDescription.name][targetZoom]}/${x}_${y}.png`
+        }
+
+        // Otherwise generate the file
+        console.log(`Setting up map ${subMapDescription.name}`)
+        exec(`python3 ../geotiff-map-exploder/setup_map.py -use-defaults ${subMapDescription.name}`, (error, stdout, stderr) => {
+            if ( error ) {
+                console.error(error)
+                exit(1)
+            }
+        })
+        console.log(`Exploding map ${subMapDescription.name}`)
+        exec(`python3 ../geotiff-map-exploder/setup_map.py -use-defaults ${subMapDescription.name}`, (error, stdout, stderr) => {
+            if ( error ) {
+                console.error(error)
+                exit(1)
+            }
+        })
+        console.log(`Explosion complete`)
     }
 
     public dispose(): void {
