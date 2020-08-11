e8f4002f2089f6af735e0df08e31b50a9f7c16f6
Jason Carter <jcarter@naturalhues.com>
2020-08-11 13:33:57 -0400

    Added content to privacy settings screen

--- vfr-green-app/app/src/main/java/com/blueskycharts/app/preferences/ManagePrivacyActivity.kt
+++ vfr-green-app/app/src/main/java/com/blueskycharts/app/preferences/ManagePrivacyActivity.kt
@@ -1,13 +1,40 @@
 package com.blueskycharts.app.preferences
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import com.blueskycharts.app.R
+import com.blueskycharts.app.map.MapViewActivity
 import com.blueskycharts.app.subscription.SubscriptionChecker
+import com.blueskycharts.app.utility.Log
 
 class ManagePrivacyActivity : SubscriptionChecker(false, true) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_privacy)
+
+        val firebaseConsentCheckmark = findViewById<CheckBox>(R.id.firebaseConsent)
+        val ctaButton = findViewById<Button>(R.id.cta)
+        ctaButton?.setOnClickListener {
+            val firebaseConsent = firebaseConsentCheckmark?.isChecked ?: false
+
+            Preferences.instance.setPreference(Preferences.propertyNameAllowFirebaseLogging, firebaseConsent)
+            Preferences.instance.setPreference(Preferences.propertyNameAllowFirebaseCrashalytics, firebaseConsent)
+
+            it.context?.let { it2 -> Log.refreshFirebaseConsent(it2) }
+
+            val intent = Intent(this, MapViewActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val firebaseConsentCheckmark = findViewById<CheckBox>(R.id.firebaseConsent)
+        val firebaseConsent = Preferences.instance.getBooleanValue(Preferences.propertyNameAllowFirebaseLogging, Preferences.defaultValueAllowFirebaseLogging)
+        val crashalyticsConsent = Preferences.instance.getBooleanValue(Preferences.propertyNameAllowFirebaseCrashalytics, Preferences.defaultValueAllowFirebaseCrashalytics)
+        firebaseConsentCheckmark.isChecked = firebaseConsent && crashalyticsConsent
     }
 }
